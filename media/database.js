e0d03c76cf09f95b5ec91bc115bea7ff386425a8
Aki <75360508+Aki-108@users.noreply.github.com>
2022-05-01 13:18:39 +0200

    Update database.js

--- media/database.js
+++ media/database.js
@@ -2209,7 +2209,9 @@ var database = [
 
 ["book", ["Die drei ??? Kids 7: Gruft der Piraten"], "2000", [["2022 04 25", ""]], 0, ["ger"], [], [], ["Die Drei ???", "bibliothek", "watching"], ["Ulf Blanck"], [], "https://64.media.tumblr.com/76b1f777dfc0fb89c5ad8c53586721e1/8d07670ca5b77157-68/s250x400/8f912755974d94d3b791d6c40d746c044071c4fb.jpg"],
 
-["film", ["Slumdog Millionaire", "Slumdog Millionär"], "2008", ["2022 04 28"], 0, ["ger"], ["drama", "romance"], [], [], ["Danny Boyle", "Loveleen Tandan"], ["Dev Patel", "Irrfan Khan"], "https://64.media.tumblr.com/8492f47674370ff6d2b90f3c7d0d52b7/8968561d519a3d9b-2d/s250x400/faecb34b9870ccb5fac446f8c79a130ef96150a8.jpg"]
+["film", ["Slumdog Millionaire", "Slumdog Millionär"], "2008", ["2022 04 28"], 0, ["ger"], ["drama", "romance"], [], [], ["Danny Boyle", "Loveleen Tandan"], ["Dev Patel", "Irrfan Khan"], "https://64.media.tumblr.com/8492f47674370ff6d2b90f3c7d0d52b7/8968561d519a3d9b-2d/s250x400/faecb34b9870ccb5fac446f8c79a130ef96150a8.jpg"],
+
+["audiobook", ["Das Haus der Mädchen"], "2018", [["2022 04 30", "2022 05 01", "C2-C3"]], 8, ["ger"], [], ["bini", "syl", "je"], [], ["Andreas Winkelmann"], ["Simon Jäger"], "https://64.media.tumblr.com/0a2b04057cafa5510dc5b7571fb0f7d2/9a81f7692f426342-77/s250x400/06f21c73bc581fa76a2b977657305217277c81be.jpg"]
 
 	
 /*
