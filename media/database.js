af7a823a28d30ca5a0ad4e9b810ddedb748ac16c
Aki <75360508+Aki-108@users.noreply.github.com>
2023-06-05 07:28:51 +0200

    Update database.js

--- media/database.js
+++ media/database.js
@@ -2595,7 +2595,7 @@ var database = [
 
 ["film", ["Girl, Interrupted", "Durchgeknallt"], "1999", ["2023 04 14"], 0, ["ger"], ["biography", "drama"], [], ["Netflix"], ["James Mangold"], ["Winona Ryder", "Angelina Jolie", "Brittany Murphy", "Whoopi Goldberg"], "https://64.media.tumblr.com/d6fa8cee8e6c7b80b4a4f3caf0e1331d/e2edf5e0b0002cb1-8f/s250x400/a4ad524a5937b6e718b61c4d6b9e180851e546b0.jpg"],
 
-["series", ["Austin & Ally"], ["2011", "2016"], [["2023 04 15", "", "S1"]], 0, ["ger"], ["comedy", "drama"], [], ["Disney+", "watching", "ongoing"], ["Kevin Kopelow"], ["Ross Lynch", "Laura Marano"], "https://64.media.tumblr.com/0249a6d48c116caefed14e41f349aa70/8adb5822370b19f8-6a/s250x400/47d63a62a3ab6688747543262a0a741bd606e2fc.jpg"],
+["series", ["Austin & Ally"], ["2011", "2016"], [["2023 04 15", "2023 06 05", "S1"]], 5, ["ger"], ["comedy", "drama"], [], ["Disney+", "ongoing"], ["Kevin Kopelow"], ["Ross Lynch", "Laura Marano"], "https://64.media.tumblr.com/0249a6d48c116caefed14e41f349aa70/8adb5822370b19f8-6a/s250x400/47d63a62a3ab6688747543262a0a741bd606e2fc.jpg"],
 
 ["film", ["Glass Onion", "Glass Onion: A Knives Out Mystery"], "2022", ["2023 04 15"], 8, ["ger"], ["comedy", "crime"], ["bini", "mai"], ["Netflix"], ["Rian Johnson"], ["Daniel Craig", "Edward Norton", "Kathryn Hahn", "Leslie Odom Jr.", "Kate Hudson", "Dave Bautista", "Jessica Henwick", "Hugh Grant", "Natasha Lyonne"], "https://64.media.tumblr.com/551fd9a459d760e2d7b577b91e8fcf8b/927f9c1c53016ba6-6d/s250x400/fb15789ba0a974bf3fe28d038031c1070b5b52b6.jpg"],
 
