fce7b81bcdf7c318a3b3a5e19c265c7f1f5b3b89
Aki <75360508+Aki-108@users.noreply.github.com>
2023-08-25 06:28:32 +0200

    Update database.js

--- media/database.js
+++ media/database.js
@@ -2367,7 +2367,7 @@ var database = [
 
 ["book", ["Die Wilden Hühner auf Klassenfahrt"], "1996", [["2022 08 16", "2022 08 30"]], 6, ["ger"], [], [], ["bibliothek", "Die Wilden Hühner"], ["Cornelia Funke"], [], "https://64.media.tumblr.com/80c66240bce785ad40d8beea915c2083/28a794b62581dc6c-da/s250x400/e9050b9aad3148a756541bfae20bb51087831411.jpg"],
 
-["series", ["Boy Meets World", "Das Leben und ich"], ["1993", "2000"], [["2022 08 21", "2022 08 29", "S1"], ["2022 08 29", "2022 09 11", "S2"], ["2022 10 03", "2022 11 06", "S3"], ["2022 11 10", "2022 12 27", "S4"], ["2023 01 29", "2023 02 05", "S5"], ["2023 04 04", "2023 04 10", "S6"], ["", "2023 08 21", "S7"]], 6, ["ger"], ["comedy", "drama"], [], ["Disney+"], ["Michael Jacobs"], ["Ben Savage", "William Daniels", "Betsy Randle", "Rider Strong", "William Russ", "Danielle Fishel", "Lindsay Ridgeway", "Anthony Tyler Quinn", "Lily Gibson", "Lee Norris"], "https://64.media.tumblr.com/fc0d57e3c3386b084d0e89b0456d7b75/6afd8f2f29acf24c-c6/s250x400/6fe3fe1189a4d09a9f5312fe92f9d7a8d33ab169.jpg"],
+["series", ["Boy Meets World", "Das Leben und ich"], ["1993", "2000"], [["2022 08 21", "2022 08 29", "S1"], ["2022 08 29", "2022 09 11", "S2"], ["2022 10 03", "2022 11 06", "S3"], ["2022 11 10", "2022 12 27", "S4"], ["2023 01 29", "2023 02 05", "S5"], ["2023 04 04", "2023 04 10", "S6"], ["2023 08 04", "2023 08 21", "S7"]], 6, ["ger"], ["comedy", "drama"], [], ["Disney+"], ["Michael Jacobs"], ["Ben Savage", "William Daniels", "Betsy Randle", "Rider Strong", "William Russ", "Danielle Fishel", "Lindsay Ridgeway", "Anthony Tyler Quinn", "Lily Gibson", "Lee Norris"], "https://64.media.tumblr.com/fc0d57e3c3386b084d0e89b0456d7b75/6afd8f2f29acf24c-c6/s250x400/6fe3fe1189a4d09a9f5312fe92f9d7a8d33ab169.jpg"],
 
 ["film", ["Z-O-M-B-I-E-S", "Zombies - Das Musical"], "2018", ["2022 08 25"], 5, ["ger"], ["family", "musical"], [], ["Disney+"], ["Paul Hoen"], ["Milo Manheim", "Meg Donnelly"], "https://64.media.tumblr.com/872f8f06df5b500939e5870fdd0d4ce4/14a080293eb78a81-04/s250x400/55213a056e85af0b98bb3456d63a3d1cb0a319bc.jpg"],
 
@@ -2696,7 +2696,7 @@ var database = [
 
 ["audiobook", ["Das mörderische Manuskript"], "2022", [["2023 08 15", "2023 08 18"]], 9, ["ger"], ["crime"], [], ["bibliothek", "Kloster, Mord und Dolce Vita - Schwester Isabella ermittelt"], ["Valentina Morelli"], ["Chris Nonnast"], "https://64.media.tumblr.com/82604a2d83e7863617796d14eb84a0d4/d9ebabab97fbcc29-02/s250x400/bc1813f423f61d082e07235f37dc3c0ffb9dbaa4.jpg"],
 
-["audiobook", ["Das Rätsel des Klosterkellers"], "2023", [["2023 08 19", ""]], 0, ["ger"], ["crime"], [], ["watching", "bibliothek", "Kloster, Mord und Dolce Vita - Schwester Isabella ermittelt"], ["Valentina Morelli"], ["Chris Nonnast"], "https://64.media.tumblr.com/6d5292a1bad982e1debffaac7f84318e/76e68c7f27aea97c-c6/s250x400/0cbd529238c7101d51db8a61f64d86003038348c.jpg"]
+["audiobook", ["Das Rätsel des Klosterkellers"], "2023", [["2023 08 19", "2023 08 24"]], 8, ["ger"], ["crime"], [], ["bibliothek", "Kloster, Mord und Dolce Vita - Schwester Isabella ermittelt"], ["Valentina Morelli"], ["Chris Nonnast"], "https://64.media.tumblr.com/6d5292a1bad982e1debffaac7f84318e/76e68c7f27aea97c-c6/s250x400/0cbd529238c7101d51db8a61f64d86003038348c.jpg"]
 
 /*
 ,
