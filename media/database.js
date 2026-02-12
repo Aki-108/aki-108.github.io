0b895af027daf95bb46e2dabc2a8029d6e6b7349
Aki <75360508+Aki-108@users.noreply.github.com>
2026-02-12 06:35:11 +0100

    Update database.js

--- media/database.js
+++ media/database.js
@@ -3134,7 +3134,7 @@ var database = [
 
 ["film", ["Twelve Monkeys", "12 Monkeys"], "1995", ["2024 11 30"], 7, ["ger"], ["mystery", "sci-fi"], [], ["RTL+"], ["Terry Gilliam"], ["Bruce Willis", "Brad Pitt", "Christopher Meloni"], "https://64.media.tumblr.com/c580a03095b0e2e8f532f934934dd607/2ce0b55b491f69a6-0d/s250x400/014a165268df127faa71349212297768bd879e47.jpg"],
 
-["series", ["Phineas and Ferb", "Phineas und Ferb"], ["2007", "2024"], [["2024 12 04", "2025 05 07", "S1-S2"], ["2025 05 14", "2025 07 30", "S3"], ["2025 07 30", "2025 12 15", "S4"], ["2025 12 15", "", "S5"]], 0, ["ger"], ["animation", "comedy"], ["bini", "nico"], ["watching", "Disney+"], ["Jeff 'Swampy' Marsh"], [], "https://64.media.tumblr.com/93c1bf121ce15f170731bf700fab1c05/fd60583ad87c43eb-bd/s250x400/a8be584bc8c05357642dff9cfc4786ff189ac345.jpg"],
+["series", ["Phineas and Ferb", "Phineas und Ferb"], ["2007", "2024"], [["2024 12 04", "2025 05 07", "S1-S2"], ["2025 05 14", "2025 07 30", "S3"], ["2025 07 30", "2025 12 15", "S4"], ["2025 12 15", "2026 01 25", "S5"]], 6, ["ger"], ["animation", "comedy"], ["bini", "nico"], ["Disney+"], ["Jeff 'Swampy' Marsh"], [], "https://64.media.tumblr.com/93c1bf121ce15f170731bf700fab1c05/fd60583ad87c43eb-bd/s250x400/a8be584bc8c05357642dff9cfc4786ff189ac345.jpg"],
 
 ["film", ["The Mitchells vs the Machines", "Die Mitchells gegen die Maschinen"], "2021", ["2024 12 07"], 9, ["ger"], ["animation", "adventure"], [], ["Netflix"], ["Michael Rianda", "Jeff Rowe"], [], "https://64.media.tumblr.com/459c36130be4fca6f00126e95da53406/810d4e5326d6c9dd-81/s250x400/de05a8faafcf915b39e837f936fb8caa196b5e13.jpg"],
 
@@ -3508,7 +3508,7 @@ var database = [
 
 ["audiobook", ["The Mystery of the Crooked Cat", "Die drei ??? 004: und die schwarze Katze"], "1970", [["2026 01 24", "2026 01 26"]], 6, ["ger"], ["crime"], [], ["YouTube", "Die drei ???"], ["William Arden", "Dennis Lynds"], ["Oliver Rohrbeck", "Jens Wawrczeck", "Andreas Fröhlich"], "https://64.media.tumblr.com/db1436c6b080e93389c12f81588d5aa3/0cb09f0a8b24077e-e1/s250x400/0c094a2f75e7441443eb452c57e58d58450cc017.jpg"],
 
-["series", ["Cat's Eye"], ["1983", "1985"], [["2026 01 25", ""]], 0, ["ger"], ["action", "comedy"], [], ["Disney+", "watching"], [], [], "https://64.media.tumblr.com/7f194c39c337b430dc988ee6f5be9d9a/b2f616f5e32afe67-f7/s250x400/b7ad8d78cc89ef42997dff8989f02c0721a509e5.jpg"],
+["series", ["Cat's Eye"], ["1983", "1985"], [["2026 01 25", "2026 01 30", "S1E1-S1E12"]], 5, ["ger"], ["action", "comedy"], [], ["Disney+", "ongoing"], [], [], "https://64.media.tumblr.com/7f194c39c337b430dc988ee6f5be9d9a/b2f616f5e32afe67-f7/s250x400/b7ad8d78cc89ef42997dff8989f02c0721a509e5.jpg"],
 
 ["audiobook", ["The Mystery of the Fiery Eye", "Die drei ??? 005: und der Fluch des Rubins"], "1967", [["2026 01 26", "2026 01 27"]], 5, ["ger"], ["crime"], [], ["YouTube", "Die drei ???"], ["Robert Arthur"], ["Oliver Rohrbeck", "Jens Wawrczeck", "Andreas Fröhlich"], "https://64.media.tumblr.com/d37fb0629a6564db1b5b46b7104c0e2d/9b3a815442cfdd0d-6d/s250x400/cc125b4be2e435a0c48e5e9a616d9ee1e9183458.jpg"],
 
